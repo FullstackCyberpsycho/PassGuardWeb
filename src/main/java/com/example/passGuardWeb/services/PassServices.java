91d23895fdc833c252a0491835337a172c23e404
Denis <denis@Denis.com>
2025-12-11 17:48:10 +1000

    Добавлено: кеширование сервисов... и их логирование

--- src/main/java/com/example/passGuardWeb/services/PassServices.java
+++ src/main/java/com/example/passGuardWeb/services/PassServices.java
@@ -5,16 +5,22 @@ import com.example.passGuardWeb.AesUtil;
 import com.example.passGuardWeb.dto.PasswordDto;
 import com.example.passGuardWeb.models.Password;
 import com.example.passGuardWeb.repository.PassRepository;
+//import jakarta.persistence.Cacheable;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.security.core.parameters.P;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 //import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PassServices {
@@ -22,20 +28,23 @@ public class PassServices {
     private final PasswordEncoder passwordEncoder;
     private final AesUtil aes;
 
+    @CacheEvict(value = "passwords", allEntries = true)
     public Password passSave(Password password, User user) {
         try {
             String encrypted = aes.encrypt(password.getPassword());
             password.setPassword(encrypted);
             password.setUser(user);
+            log.info("Учетная запись {} добавлена", password.getServiceName());
             return passRepository.save(password);
         } catch (Exception e) {
+            log.warn("Неудачная попытка добавить учетную запись {}", password.getServiceName());
             throw new RuntimeException("Ошибка шифрования", e);
         }
     }
 
+    @Cacheable(value = "passwords")
     public List<PasswordDto> getAllPasswords(Long userId) {
         List<Password> list = passRepository.findByUserId(userId);
-
         List<PasswordDto> result = new ArrayList<>(list.size());
 
         for (Password p : list) {
@@ -53,33 +62,39 @@ public class PassServices {
         return result;
     }
 
+    @CacheEvict(value = "passwords", allEntries = true)
     public boolean deleteUserPassword(Long userId, Long passwordId) {
         // Проверим, существует ли пароль у этого пользователя
         Optional<Password> pass = passRepository.findById(passwordId);
         if (pass.isPresent() && pass.get().getUser().getId().equals(userId)) {
             passRepository.deleteByIdAndUserId(passwordId, userId);
+            log.info("Учетная запись под id={}, пользователя id={} была удалена" , pass.get().getId(), userId);
             return true;
         }
+        log.warn("Неудачная попытка удалить учетную запись {}", pass.get().getServiceName());
         return false;
     }
 
-    public Password updatePassword(Long userId, Long passwordId, PasswordDto dto) {
+    @CacheEvict(value = "passwords", allEntries = true)
+    public Password updatePassword(Long userId, Long passwordId, PasswordDto passwordDto) {
 
         // Находим пароль, который принадлежит именно ЭТОМУ пользователю
         Password password = passRepository
                 .findByIdAndUserId(passwordId, userId)
-                .orElseThrow(() -> new RuntimeException("Password not found or access denied"));
+                .orElseThrow(() -> new RuntimeException("Учетная запись не найдена"));
 
-        password.setServiceName(dto.getServiceName());
-        password.setWebsite(dto.getWebsite());
-        password.setUsername(dto.getUsername());
+        password.setServiceName(passwordDto.getServiceName());
+        password.setWebsite(passwordDto.getWebsite());
+        password.setUsername(passwordDto.getUsername());
 
-        String encrypted = aes.encrypt(dto.getPassword());
+        String encrypted = aes.encrypt(passwordDto.getPassword());
         password.setPassword(encrypted);
 
+        log.info("Учетная запись {} обновлена", passwordDto.getServiceName());
         return passRepository.save(password);
     }
 
+    @Cacheable(value = "passwords", key = "'serviceName_' + #serviceName")
     public List<PasswordDto> getSearchPassword(Long userId, String serviceName) {
         return passRepository.findByUserIdAndServiceNameStartingWithIgnoreCase(userId, serviceName.trim()).stream()
                 .map(p -> new PasswordDto(
@@ -91,6 +106,7 @@ public class PassServices {
                 )).toList();
     }
 
+    @Cacheable(value = "passwords", key = "'ascSortedPassword'")
     public List<PasswordDto> getAscSortedPassword(Long userId) {
         return passRepository.findByUserIdOrderByServiceNameAsc(userId)
                 .stream().map(p -> new PasswordDto(
@@ -102,6 +118,7 @@ public class PassServices {
                 )).toList();
     }
 
+    @Cacheable(value = "passwords", key = "'descSortedPassword'")
     public List<PasswordDto> getDescSortedPassword(Long userId) {
         return passRepository.findByUserIdOrderByServiceNameDesc(userId)
                 .stream().map(p -> new PasswordDto(
--- src/main/java/com/example/passGuardWeb/services/UserServices.java
+++ src/main/java/com/example/passGuardWeb/services/UserServices.java
@@ -4,13 +4,18 @@ import com.example.auth.models.User;
 import com.example.passGuardWeb.repository.UserRepository;
 import com.example.passGuardWeb.dto.UserInfoDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServices {
     private final UserRepository userRepository;
 
+    @Cacheable(value = "users")
     public UserInfoDto getNameAndUsername(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -18,6 +23,7 @@ public class UserServices {
        return new UserInfoDto(user.getName(), user.getUsername(), user.getName().substring(0, 1).toUpperCase());
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public UserInfoDto update(Long userId, UserInfoDto userInfoDto) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -25,10 +31,13 @@ public class UserServices {
         user.setUsername(userInfoDto.getUsername());
         userRepository.save(user);
 
+        log.info("Пользователь с id={} обновлен", userId);
         return new UserInfoDto(user.getName(), user.getUsername(), user.getName().substring(0, 1).toUpperCase());
     }
 
-    public void deleteById(Long userID) {
-        userRepository.deleteById(userID);
+    @CacheEvict(value = "users", allEntries = true)
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
+        log.info("Пользователь с id={} удален", userId);
     }
 }
